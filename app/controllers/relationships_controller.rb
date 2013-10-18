cbc5bbfac7c9197027d0989dfc6628c1ca005c58
Graham Wert <grahamwert@gmail.com>
2013-10-18 17:36:58 -0400

    Chapter 11 Exercises

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,26 @@
 class RelationshipsController < ApplicationController
   before_action :signed_in_user
 
+  respond_to :html, :js
+
+  def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    respond_with @user
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    respond_with @user
+  end
+end
+
+#Alternate implementation
+=begin
+class RelationshipsController < ApplicationController
+  before_action :signed_in_user
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
@@ -19,3 +39,4 @@ class RelationshipsController < ApplicationController
     end
   end
 end
+=end
